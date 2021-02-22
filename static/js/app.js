b2388e4d7de6cd7352fe847809dcbb1c1ce3bf5c
kehull <71107280+kehull@users.noreply.github.com>
2021-02-21 18:25:29 -0600

    trying a different direction

--- static/js/app.js
+++ static/js/app.js
@@ -11,7 +11,7 @@ var button = d3.select("#button");
 var form = d3.select("form");
 
 // Console.log the ufo data from data.js
-console.log(data);
+// console.log(data);
 
 // BONUS: Refactor to use Arrow Functions!
 data.forEach((ufoInfo) => {
@@ -24,26 +24,44 @@ data.forEach((ufoInfo) => {
 
 button.on("click", runEnter);
 form.on("submit",runEnter);
-var filteredData = []
-// Complete the event handler function for the form
-function runEnter() {
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-    data.forEach((sighting) => {
-    Object.entries(sighting).forEach(([key, value]) => {
-        if (sighting.datetime === inputValue) {
-                filteredData.push(sighting);
-                console.log(sighting);
-        }
-      });
-    });
-    tbody.text(filteredData);
-};
 
-// currently prints entire filteredData array in first cell of table and is unreadable.
-// need to figure out how to convert array to table so it can be viewed on the page
-// and then repeat the processes for the city, state, country, and shape filters
+function runEnter() {
+  d3.event.preventDefault();
+  var inputElement = document.getElementById("datetime");
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+  console.log(filteredData);
+}
+
+
+
+// var filteredData = []
+// // Complete the event handler function for the form
+// function runEnter() {
+//     // Prevent the page from refreshing
+//     d3.event.preventDefault();
+//     // Select the input element and get the raw HTML node
+//     var inputElement = d3.select("#datetime");
+//     // Get the value property of the input element
+//     var inputValue = inputElement.property("value");
+//     data.forEach((instance) => {
+//       Object.entries(instance).forEach(([key, value]) => {
+//         if (instance.datetime === inputValue) {
+//           data.forEach((i) => {
+//             console.log(instance);
+//             var row = tbody.append("tr");
+//             Object.entries(innerWidth).forEach(([key, value]) => {
+//               var cell = row.append("td");
+//               cell.text(value);
+//           });
+//         }
+//       )};
+//     }
+//   )}
+// )};
+
+// // filtering works correctly, need to append to html table
