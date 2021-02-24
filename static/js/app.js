229183b5c915b604ac629c0c79e2b63183fcf9ca
kehull <71107280+kehull@users.noreply.github.com>
2021-02-24 17:14:52 -0600

    after looking over code with tutor

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,12 @@
 // from data.js
 var tableData = data;
 
-// Get a reference to the table body
+// Get a reference to the table body, select the button and  form
 var tbody = d3.select("tbody");
-
-// Select the button
-var button = d3.select("#button");
-
-// Select the form
+var button = d3.select("#filter-btn");
 var form = d3.select("form");
 
-// Console.log the ufo data from data.js
-// console.log(data);
-
-// BONUS: Refactor to use Arrow Functions!
+// read data.js into the page
 data.forEach((ufoInfo) => {
   var row = tbody.append("tr");
   Object.entries(ufoInfo).forEach(([key, value]) => {
@@ -22,31 +15,28 @@ data.forEach((ufoInfo) => {
   });
 });
 
+// tell the buttons how to behave
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
+// allow filtering
 function runEnter() {
-  d3.event.preventDefault();
-  var inputElement = document.getElementById("datetime");
-  var inputValue = inputElement.value;
-  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-  // console.log(filteredData); // returns each sighting as an object
+  d3.event.preventDefault(); // prevent the page from refreshing
+  var inputElement = document.getElementById("datetime"); // get datetime info for the current row
+  var inputValue = inputElement.value; //establish the use input as a variable
+  var filteredData = tableData.filter(sighting => {
+    return sighting.datetime === inputValue;
+  }); // filter data, only keep data that matches the input value
+  tbody.html(""); // empty the data from the html table
   filteredData.forEach((instance) => {
-    // console.log(instance); returns each sighting object
-    newInfo = Object.entries(instance);
-    // console.log(newInfo)  // returns each object as a list of lists where each list is a key/value pair. 
-      //i thought this was the way to go based on intro to javascript/3/activities/02, but i can't get it to work past here
-    newInfo.forEach(([datetime, city, country, shape, durationMinutes, comments]) => {
-      var row = tbody.append("tr");
-      row.append("td").text(datetime); // using console.log() for each of the items in this section shows that they're calling a bunch of stuff,
-        // not just one cell's worth of text
-      row.append("td").text(city);
-      row.append("td").text(country);
-      row.append("td").text(shape);
-      row.append("td").text(durationMinutes);
-      row.append("td").text(comments);
+    console.log(instance);
+    var row = tbody.append("tr"); // append each filtered row to the table
+    const newInfo = Object.values(instance);
+    console.log(newInfo)  
+      newInfo.forEach(function(info) {
+        console.log(info);
+        row.append("td").text(info); 
       });
     }
-  )};
-
-  // after running this code, it appends a bunch of rows of key/value pairs at the bottom of the existing table.
+  )
+};
